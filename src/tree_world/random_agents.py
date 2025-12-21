bc2328cb627b5046aae39600c70dcac94c51ba46
Alan Lockett <alockett@Alans-MacBook-Pro.local>
2025-12-21 11:11:21 -0600

    switch to true VAE

--- src/tree_world/random_agents.py
+++ src/tree_world/random_agents.py
@@ -190,8 +190,8 @@ class RandomTemTAgent(AgentModel):
         self.last_location = torch.cat([location_prefix[None, ...], self.last_location[:, -self.buffer:]], dim=1)
         self.last_sensory = torch.cat([sensory_prefix[None, ...], self.last_sensory[:, -self.buffer:]], dim=1)
         self.last_action = [x[None, ...] for x in action_prefix] + self.last_action[-self.buffer:]
-        self.salience_scores = [x for x in torch.cat([salience_score_prefix, self.salience_scores[-self.buffer:]], dim=0)]
-        self.rewards = [x for x in torch.cat([reward_prefix, self.rewards[-self.buffer:]], dim=0)]
+        self.salience_scores = [x for x in salience_score_prefix] + self.salience_scores[-self.buffer:]
+        self.rewards = [x for x in reward_prefix] + self.rewards[-self.buffer:]
     
     @classmethod
     def from_config(cls, config: 'TreeWorldConfig'):
