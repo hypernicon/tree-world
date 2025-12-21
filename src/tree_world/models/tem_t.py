f3430164d0d58229972499a3d4c199919deefe85
Alan Lockett <alockett@Alans-MacBook-Pro.local>
2025-12-21 16:14:04 -0600

    switch to true VAE

--- src/tree_world/models/tem_t.py
+++ src/tree_world/models/tem_t.py
@@ -225,7 +225,7 @@ class MetricSampler(torch.nn.Module):
         sampled_value_distances = self.v_metric.cross_distance(value, value_mean, squared=True, scale=scale / (v_std + 1e-8))
         sampled_value_probs = torch.exp(-0.5 * sampled_value_distances) # B, T, S
         core_logprobs = torch.log((qk_weights * sampled_value_probs).sum(dim=-1) + 1e-8)  # B, T
-        std_log_probs = torch.log(v_std + 1e-8).sum(dim=-1) # B, T
+        std_log_probs = torch.log(v_std[:, -T:] + 1e-8).sum(dim=-1) # B, T
         logprobs = core_logprobs - 0.5 * math.log(2 * math.pi) * E - std_log_probs # B, T
         return logprobs
 
