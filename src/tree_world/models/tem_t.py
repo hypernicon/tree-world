58ca1303bfa7e26e681a9a3ab3df18202aa1e15f
Alan Lockett <alockett@Alans-MacBook-Pro.local>
2025-12-21 11:59:17 -0600

    switch to true VAE

--- src/tree_world/models/tem_t.py
+++ src/tree_world/models/tem_t.py
@@ -404,8 +404,9 @@ class TemLocalizer(torch.nn.Module):
         )
 
         kl_divergence = sensory_location_logprobs - geometric_logprobs
-        kl_divergence = kl_divergence.masked_fill(location_invalid_mask[:, prefix_length:], 0.0)
-        kl_divergence = kl_divergence.sum(dim=-1) / ((~location_invalid_mask).to(kl_divergence.dtype).sum(dim=-1) + 1e-8)
+        mask = torch.isnan(kl_divergence) | torch.isinf(kl_divergence) | location_invalid_mask[:, prefix_length:]
+        kl_divergence = kl_divergence.masked_fill(mask, 0.0)
+        kl_divergence = kl_divergence.sum(dim=-1) / ((~mask).to(kl_divergence.dtype).sum(dim=-1) + 1e-8)
         if torch.isnan(kl_divergence).any() or torch.isinf(kl_divergence).any():
             print(f"kl_divergence is nan: {kl_divergence.isnan().float().sum()} out of {kl_divergence.numel()}")
             print(f"kl_divergence is inf: {kl_divergence.isinf().float().sum()} out of {kl_divergence.numel()}")
