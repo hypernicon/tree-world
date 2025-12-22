585b523a7aaa35a0a860317054ed8308b4120fcd
Alan Lockett <alockett@Alans-MacBook-Pro.local>
2025-12-21 22:49:30 -0600

    switch to true VAE

--- src/tree_world/models/tem_t.py
+++ src/tree_world/models/tem_t.py
@@ -170,7 +170,7 @@ class MetricSampler(torch.nn.Module):
         # scale = self.scale_factor ** 0.5 / (op_norm ** 0.5 + 1e-8)
         scale = self.scale_factor ** 0.5
 
-        qk_distances = self.qk_metric.cross_distance(query, key, squared=True, scale=scale).clamp(max=1e4)
+        qk_distances = self.qk_metric.cross_distance(query, key, squared=True, scale=scale)
         if qk_distances.isnan().any():
             print(f"qk_distances is nan: {qk_distances.isnan().float().sum()} out of {qk_distances.numel()}")
             print(f"query: {query[0,:4, :10].detach().float().cpu().numpy().tolist()}")
@@ -221,8 +221,7 @@ class MetricSampler(torch.nn.Module):
         # compute the log probability of sampled_value
         # this is a mixture of gaussians, so unfortunately we can't use a simple log probability formula
         # how far is sampled_value from EVERY value? (B, T, S)
-        scale = value_mean.shape[-1] ** -0.5
-        sampled_value_distances = self.v_metric.cross_distance(value, value_mean, squared=True, scale=scale / (v_std + 1e-8)).clamp(max=1e4)
+        sampled_value_distances = ((value - value_mean[:, -T:]) / (v_std[:, -T:] + 1e-8)).pow(2).sum(dim=-1)
         sampled_value_probs = torch.exp(-0.5 * sampled_value_distances) # B, T, S
         core_logprobs = torch.log((qk_weights * sampled_value_probs).sum(dim=-1) + 1e-8)  # B, T
         std_log_probs = torch.log(v_std[:, -T:] + 1e-8).sum(dim=-1) # B, T
