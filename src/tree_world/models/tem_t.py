b43174348332468e1063537044d5d241c0a124a2
Alan Lockett <alockett@Alans-MacBook-Pro.local>
2025-12-21 22:59:43 -0600

    switch to true VAE

--- src/tree_world/models/tem_t.py
+++ src/tree_world/models/tem_t.py
@@ -222,7 +222,7 @@ class MetricSampler(torch.nn.Module):
         # this is a mixture of gaussians, so unfortunately we can't use a simple log probability formula
         # how far is sampled_value from EVERY value? (B, T, S)
         sampled_value_distances = ((value[:, :, None, :] - value_mean[:, None, :, :]) / (v_std[:, None, :, :] + 1e-8)).pow(2).sum(dim=-1)
-        sampled_value_probs = torch.exp(-0.5 * sampled_value_distances)  / v_std.prod(dim=-1) # B, T, S
+        sampled_value_probs = torch.exp(-0.5 * sampled_value_distances)  / (v_std.prod(dim=-1) + 1e-8) # B, T, S
         core_logprobs = torch.log((qk_weights * sampled_value_probs).sum(dim=-1) + 1e-8)  # B, T
         # std_log_probs = torch.log(v_std + 1e-8).sum(dim=-1) # B, T
         logprobs = core_logprobs - 0.5 * math.log(2 * math.pi) * E # - std_log_probs # B, T
