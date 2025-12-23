d3be7cf6eb1048d2feef814af516c916e4f41817
Alan Lockett <alockett@Alans-MacBook-Pro.local>
2025-12-23 11:26:32 -0600

    switch to bulk training

--- src/tree_world/models/metric.py
+++ src/tree_world/models/metric.py
@@ -434,7 +434,6 @@ def gather_component(
         out = x.gather(dim=comp_dim, index=gather_idx).squeeze(-1)
         return out
     elif x.ndim == idx.ndim + 2:
-        print(f"x: {x.shape}, idx: {idx.shape}")
         # x: (..., S, E)
         B, Tx, S, E = x.shape
         B, T = idx.shape
