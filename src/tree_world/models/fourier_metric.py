5e0d5f76c82f95989d829263fc39e2c3a8b6637b
Alan Lockett <alockett@Alans-MacBook-Pro.local>
2025-12-25 12:22:08 -0600

    rework to add explicit fourier sampler and generic mixture

--- src/tree_world/models/fourier_metric.py
+++ src/tree_world/models/fourier_metric.py
@@ -20,7 +20,7 @@ def check_valid_location(location: torch.Tensor, batch_lengths: Optional[torch.T
         while batch_mask.ndim < location.ndim:
             batch_mask = batch_mask[..., None]
         batch_mask = batch_mask.expand_as(location)
-        batch_mask = batch_mask.reshape(-1, 2).max(dim=-1).values
+        batch_mask = batch_mask.max(dim=-1).values.reshape(-1)
         location_norms = torch.masked_fill(location_norms, batch_mask, 1.0)
 
     if not torch.allclose(location_norms, torch.ones_like(location_norms), atol=1e-1):
